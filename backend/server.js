e569bdefc3ce1ece637c7844b9ecaca9ef6a5260
Yuvraj <fcyuvraj18@gmail.com>
2025-10-21 02:01:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://auth-1-gw9t.onrender.com",
   credentials: true  // CRITICAL: Allows cookies // to recieve cookies from the frontend (it needs a specified origin)
 }));
 app.use(cookieParser()) // for stroing the jwt token in a cookie
